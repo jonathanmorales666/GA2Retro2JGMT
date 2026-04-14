9578d855c337eda56478d7637050f1b496e56a98
Jonathan Morales <jonathanmorales@emilianisomascos.edu.gt>
2026-04-14 08:44:55 -0600

    Se me olvidó el ejercicio 9 jaja, ejercicio #9 terminado y documentación actualizada

---
+++ EJ9_JGMT.py
@@ -0,0 +1,6 @@
+# Jonathan Gabriel Morales Torres "10"
+# Retroalimentación GA2
+# Analisis de sistemas
+
+cesta = input('Introduce los productos de la cesta de la compra separados por comas: ')
+print('\n'.join(cesta.split(',')))
