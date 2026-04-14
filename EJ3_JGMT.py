ff78dcd214be7723bfdb5a37e9f0cb259b9f0e3d
Jonathan Morales <jonathanmorales@emilianisomascos.edu.gt>
2026-04-14 08:31:46 -0600

    Ejercicio #3 terminado

--- EJ3_JGMT.py
+++ EJ3_JGMT.py
@@ -1,3 +1,6 @@
 # Jonathan Gabriel Morales Torres "10"
 # Retroalimentación GA2
 # Analisis de sistemas
+
+nombre = input("¿Como te llamas? ")
+print(nombre.upper() + " tiene " + str(len(nombre)) + " letras")
