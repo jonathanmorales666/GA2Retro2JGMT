dbb4174237efaef86099ac9c7479c824ae7130c8
Jonathan Morales <jonathanmorales@emilianisomascos.edu.gt>
2026-04-14 08:35:30 -0600

    Ejercicio #6 terminado

--- EJ6_JGMT.py
+++ EJ6_JGMT.py
@@ -1,3 +1,6 @@
 # Jonathan Gabriel Morales Torres "10"
 # Retroalimentación GA2
 # Analisis de sistemas
+
+email = input("Introduce tu correo electrónico: ")
+print(email[:email.find('@')] + '@edu.gt')
