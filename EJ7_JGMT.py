e63f41eb82d49f55107ab88b8fec3fc7f54fa080
Jonathan Morales <jonathanmorales@emilianisomascos.edu.gt>
2026-04-14 08:38:19 -0600

    Ejercicio #7 y #8 terminado

--- EJ7_JGMT.py
+++ EJ7_JGMT.py
@@ -1,3 +1,6 @@
 # Jonathan Gabriel Morales Torres "10"
 # Retroalimentación GA2
 # Analisis de sistemas
+
+precio = input("Introduce el precio del producto con dos decimales: ")
+print(precio[:precio.find('.')], 'quetzales y', precio[precio.find('.')+1:], 'centimos. ')
--- EJ8_JGMT.py
+++ EJ8_JGMT.py
@@ -1,3 +1,8 @@
 # Jonathan Gabriel Morales Torres "10"
 # Retroalimentación GA2
 # Analisis de sistemas
+
+fecha = input("Introduce la fecha de tu nacimiento en fotmato dd/mm/aaaa: ")
+print("Dia ",fecha[:2] )
+print("Mes ",fecha[3:2] )
+print("Año ",fecha[6:] )
