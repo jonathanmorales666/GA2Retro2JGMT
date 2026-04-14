e46de658ac6a4fd4d3e9bc59614d434f5b08640e
Jonathan Morales <jonathanmorales@emilianisomascos.edu.gt>
2026-04-14 08:30:52 -0600

    Ejercicio #1 y #2 terminado

--- EJ1_JGMT.py
+++ EJ1_JGMT.py
@@ -1,3 +1,7 @@
 # Jonathan Gabriel Morales Torres "10"
 # Retroalimentación GA2
 # Analisis de sistemas
+
+nombre = input("¿Cómo te llamas: ")
+n = input("Introduce un número entero: ")
+print((nombre + "\n") *int(n))
--- EJ2_JGMT.py
+++ EJ2_JGMT.py
@@ -1,3 +1,8 @@
 # Jonathan Gabriel Morales Torres "10"
 # Retroalimentación GA2
 # Analisis de sistemas
+
+name = input("¿Cómo te llamas?: ")
+print(name.lower())
+print(name.upper())
+print(name.title())
