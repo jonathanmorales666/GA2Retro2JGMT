136168174bb787cdc18ccafb86b8289a19b59815
Jonathan Morales <jonathanmorales@emilianisomascos.edu.gt>
2026-04-14 08:34:09 -0600

    Quinto ejercicio terminado

--- EJ5_JGMT.py
+++ EJ5_JGMT.py
@@ -1,3 +1,8 @@
 # Jonathan Gabriel Morales Torres "10"
 # Retroalimentación GA2
 # Analisis de sistemas
+
+frase = input("Introduce una frase: ")
+vocal = input("Introduce una vocal en minúscula:")
+print(frase.replace(vocal, vocal. upper() ))
+
